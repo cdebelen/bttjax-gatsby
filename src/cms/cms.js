80b1701068ba6c96c4f10fee7054f481ce4e1084
Christopher DeBelen <chrisdebelen@gmail.com>
2021-01-30 22:15:28 -0500

    Update CMS file

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,8 @@ import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import AboutPagePreview from "./preview-templates/InstructorsPagePreview";
+import InstructorsPagePreview from "./preview-templates/InstructorsPagePreview";
+import ClassesPagePreview from "./preview-templates/ClassesPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
@@ -12,7 +13,8 @@ CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
-CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("instructors", InstructorsPagePreview);
+CMS.registerPreviewTemplate("classes", ClassesPagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
 CMS.registerPreviewTemplate("schedule", SchedulePagePreview);
